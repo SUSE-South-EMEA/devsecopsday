95f03cc9dce84dfa2f77d5ca2b55b50c83682275
Luca Barzè <luca.barze@suse.com>
2023-08-03 16:23:39 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use the SUSE BCI Python 3.11 image as a parent image
+FROM registry.suse.com/bci/python:3.11
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Add the current directory contents into the container at /app
+ADD . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+import requests
+
+app = Flask(__name__)
+
+# MySQL configurations
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mypassword'
+app.config['MYSQL_DATABASE_DB'] = 'mydb'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+mysql.init_app(app)
+
+con = mysql.connect()
+cur = con.cursor()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/db')
+def db_query():
+    cur.execute('''SELECT data FROM mytable''')
+    rv = cur.fetchone()
+    return str(rv)
+
+def init_db():
+    cur.execute('CREATE TABLE IF NOT EXISTS mytable (id INT AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255) NOT NULL)')
+    cur.execute('SELECT * FROM mytable')
+    data = cur.fetchall()
+    if len(data) == 0:
+        for i in range(1, 4):
+            cur.execute(f"INSERT INTO mytable (data) VALUES ('DB data {i}')")
+        con.commit()
+
+init_db()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
